2f052cd2ad2e1c7ac969fc4763593f1afd18ec6d
Henry Morrison-Jones <henry-mj@hotmail.com>
2018-09-29 18:57:57 +1200

    UrgencyViewController Class documentation added.

--- 345Project/345Project/UI Redesign/UrgencyController.swift
+++ 345Project/345Project/UI Redesign/UrgencyController.swift
@@ -9,23 +9,36 @@
 import UIKit
 import CoreData
 
+/**
+ UIViewController Subclass, responsible for controlling the Urgency View.
+ 
+ View contains 3 buttons, referred to via the outlets.
+ Clicking any single button will segue back to the NewItem view, setting the task data along with
+ the updated urgency.
+ */
 class UrgencyController: UIViewController {
     
+    //MARK: Managed Context for Core Data use.
     var managedContext: NSManagedObjectContext!
+    
+    //Edit Task boolean.
     var editTask = false;
     
+    //MARK: Task variables.
     var taskDesc = "" //String for task title
     var urgency = "c" // a, b, c representing urgency from high to low
     var reminderDate = Date() // Will be a date or null.
     var time: Float = 1.0;  // hours as a decimal. eg, 3.5 = 3 hours 30 minutes.
-
+    
+    //MARK: Button and view component outlets.
     @IBOutlet weak var highButton: UIButton!
     @IBOutlet weak var medButton: UIButton!
     @IBOutlet weak var lowButton: UIButton!
     @IBOutlet weak var background: UIView!
+    
     /** All three of these functions are almost the same.
-        If a button is pressed, perform a segue using that buttons identifier.
-     **/
+        If a button is pressed, perform a segue using that button's identifier.
+    */
     @IBAction func HighButton(_ sender: UIButton) {
         performSegue(withIdentifier: "high", sender: sender)
     }
@@ -49,7 +62,7 @@ class UrgencyController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /** we are only segueing to once controller so we update all variables first.
+    /** We are only segueing to one controller so we update all variables first.
         Update the urgency variable depending on which button what clicked.
     **/
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
